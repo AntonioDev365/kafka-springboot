884cbcd3286186b461c33cfc77a36f1761c1b211
Antonio Navas <80464664+AntonioDev365@users.noreply.github.com>
2022-11-29 20:03:10 +0100

    Added the producer and the topic configuration

---
+++ src/main/java/com/antonio/config/KafkaProducerConfig.java
@@ -0,0 +1,40 @@
+package com.antonio.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    public Map<String, Object> producerConfig(){
+        Map<String,Object> properties=new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return properties;
+    }
+    //Producer factory responsible for create producer instances, allows us to create kafka producers
+    @Bean
+    public ProducerFactory<String,String> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+    //Produce messages, pass the producerFactory in the parameters with dependency injection
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String,String> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
